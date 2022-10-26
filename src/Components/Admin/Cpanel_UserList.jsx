ea2bd3c0b5a054eb98ff4c7abe66e69d3b77caa0
Asif Ali <asifmzn13@gmail.com>
2022-10-26 10:41:18 +0530

    adding files

---
+++ src/Components/Admin/Cpanel_UserList.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Cpanel_LeftNav from './Cpanel_LeftNav'
+export default function Cpanel_UserList() {
+    async function deleteRecord(_id){
+        alert("Deleteed "+_id)
+    }
+    return (
+        <div className='row'>
+            <div className='col-xxl-2 col-xl-2 col-lg-3 col-md-4 col-sm-6 col-12'>
+                <Cpanel_LeftNav />
+            </div>
+            <div className='col-xxl-10 col-xl-10 col-lg-9 col-md-8 col-sm-6 col-12'>
+                <h5 className='background text-light text-center p-2'>User List Page </h5>
+                <div className='table-responsive'>
+                    <table className='table'>
+                        <tbody>
+                            <tr>
+                                <th>Id</th>
+                                <th>Name</th>
+                                <th>User Name</th>
+                                <th>Email Id</th>
+                                <th>Phone Number</th>
+                                <th>Role</th>
+                                <th></th>
+                            </tr>
+                          
+                          
+                          
+                          
+                            <tr>
+                                <td>1</td>
+                                <td>Asif Ali</td>
+                                <td>Asifup12</td>
+                                <td>asifmzn13@gmail.com</td>
+                                <td>8868873958</td>
+                                <td>Admin</td>
+                                <td><button onClick={()=>deleteRecord(1)} className='btn text-primary'><i className="material-icons">delete_forever</i></button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    )
+}
